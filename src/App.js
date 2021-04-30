1b7ac017493f67987abde883ed459fc9a1cb7218
Patryk Przybyło <patryk13342@o2.pl>
2021-04-30 12:08:26 +0200

    lab 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
-
+import {Route, Switch} from "react-router-dom";
 import Posts from "./components/posts";
+import Home from "./components/home";
+import NavBar from "./components/common/navbar";
+import NotFound from "./components/notFound";
 
 function App() {
-  return (
-    <div className="container">
-      <Posts/>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <NavBar/>
+            <div className="container">
+                <div className="content">
+                    <Switch>
+                        <Route path="/posts"
+                               component={Posts}/>}
+                        />
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/" component={NotFound}/>
+                    </Switch>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
